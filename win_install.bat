6efb35784580f76624f94baa49489c555c433f83
Aditya Mitra <adityaarghya0@gmail.com>
2025-01-30 07:41:20 +0530

    Update win_install.bat

--- win_install.bat
+++ win_install.bat
@@ -2,9 +2,12 @@
 
 dism /online /enable-feature /featurename:Microsoft-Windows-Subsystem-Linux /all /norestart
 dism /online /enable-feature /featurename:VirtualMachinePlatform /all /norestart
+dism /online /Disable-Feature /FeatureName:IIS-WebServer
 shutdown -r -f -t 0
 :: The PC will restart now
 
+net stop w3svc
+net stop http
 wsl --install -d Ubuntu
 :: Setup username and password if asked
 
@@ -13,4 +16,7 @@ wget -O - https://AdityaMitra5102.github.io/LocalLLM/oneclickinstall.sh | sh
 
 
 :: To start it later
+
+net stop w3svc
+net stop http
 wsl -d Ubuntu
